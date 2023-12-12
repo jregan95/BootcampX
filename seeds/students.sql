5dffa714a5c00110db0f2f0a8e8c556db8c14dbc
Jill Regan <jillian.jnr@hotmail.com>
2023-12-12 17:02:25 -0500

    Add teachers and assitiance request tables

--- seeds/students.sql
+++ seeds/students.sql
@@ -1,4 +1,4 @@
-INSERT INTO students (id, name, email, phone, github, start_date, end_date, cohort_id) VALUES (1, 'Armand Hilll', 'lera_hahn@dickens.org', '778-349-3299', 'aspernatur', '2018-02-12T08:00:00.000Z', '2018-04-20T07:00:00.000Z', 1);
+ INSERT INTO students (id, name, email, phone, github, start_date, end_date, cohort_id) VALUES (1, 'Armand Hilll', 'lera_hahn@dickens.org', '778-349-3299', 'aspernatur', '2018-02-12T08:00:00.000Z', '2018-04-20T07:00:00.000Z', 1);
 INSERT INTO students (id, name, email, phone, github, start_date, end_date, cohort_id) VALUES (2, 'Stephanie Wolff', 'darius.homenick@tod.ca', '778-125-2216', 'rem', '2018-02-12T08:00:00.000Z', '2018-04-20T07:00:00.000Z', 1);
 INSERT INTO students (id, name, email, phone, github, start_date, end_date, cohort_id) VALUES (3, 'Stan Miller', 'mcdermott.maxie@schoen.com', '086-503-5484', 'qui', '2018-02-12T08:00:00.000Z', '2018-04-20T07:00:00.000Z', 1);
 INSERT INTO students (id, name, email, phone, github, start_date, end_date, cohort_id) VALUES (4, 'Elliot Dickinson', 'derrick_pollich@gmail.com', '778-848-9923', 'dolore', '2018-02-12T08:00:00.000Z', '2018-04-20T07:00:00.000Z', 1);
